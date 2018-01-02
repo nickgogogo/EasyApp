b999fa4239369fbe838075c4ef033496d1eda473
acu17hz <acu17hz@dyn103151.shef.ac.uk>
2018-01-02 16:57:27 +0000

    Initial Commit

---
+++ EasyApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  EasyApp
+//
+//  Created by acu17hz on 02/01/2018.
+//  Copyright © 2018 acu17hz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
